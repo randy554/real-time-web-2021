a5f99d7bd0f3b9530a3a9d50bcaeb5a115c266b0
randy554 <randy554@hotmail.com>
2021-04-09 14:04:28 +0200

    basic socket setup

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
-dotenv.config();
+import { createServer } from "http";
+import { Server } from "socket.io";
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 2021;
+
+// connect express to http port
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 app.use(express.static(path.resolve("public")));
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.get("/", (req, res) => {
-  res.send("hello real-time world!");
+io.on("connection", (socket) => {
+  console.log("a user has connected");
 });
 
-app.listen(port, () => {
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+httpServer.listen(port, () => {
   console.log(`Open page @ http://localhost:${port}`);
 });
